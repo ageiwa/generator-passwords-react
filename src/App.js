ec0cbc5ab42d2cb8a1b4108d20493bfb15ae6aa4
vlad ageysha <ander123fakk@gmail.com>
2021-02-27 16:07:31 +0300

    add title app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
+import TitleApp from './components/TitleApp/index';
 
 const App = () => {
     return (
-        <div className="app"></div>
+        <div className="app">
+            <TitleApp />
+        </div>
     );
 }
 
 export default App;
-
---
+++ src/components/TitleApp/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const TitleApp = () => {
+    return (
+        <div className="title-app-container">
+            <p className="title-app-container__title">Generate Password</p>
+        </div>
+    );
+}
+
+export default TitleApp;
