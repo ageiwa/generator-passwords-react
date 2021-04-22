6298f0a64ac773d86f9e3cf39365ecb3c26b7cc7
vlad ageysha <ander123fakk@gmail.com>
2021-04-22 19:29:16 +0300

    add component Password and change colors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './style.css';
 import TitleApp from './components/TitleApp/index';
+import Password from './components/Password/index';
 
 const App = () => {
     return (
         <div className="app">
             <TitleApp />
+            <div className="wrapper">
+                <Password />
+            </div>
         </div>
     );
 }
---
+++ src/components/Password/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Password = () => {
+    return (
+        <div className="password-container">
+            <p className="password">PASSWORD</p>
+        </div>
+    );
+}
+
+export default Password;
