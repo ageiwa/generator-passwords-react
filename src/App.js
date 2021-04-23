8fc6b44be88fc01f96db6bf5e090d6ef91b1932c
vlad ageysha <ander123fakk@gmail.com>
2021-04-23 10:43:29 +0300

    change name TitleApp to Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './style.css';
-import TitleApp from './components/TitleApp/index';
+import Header from './components/Header/index';
 import Password from './components/Password/index';
 
 const App = () => {
     return (
         <div className="app">
-            <TitleApp />
+            <Header />
             <div className="wrapper">
                 <Password />
             </div>
