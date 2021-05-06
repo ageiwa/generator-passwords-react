963e88fe50e760ac56f18a757633bc586ffd3760
vlad ageysha <ander123fakk@gmail.com>
2021-05-06 11:31:03 +0300

    styling BtnGenerate

--- src/components/BtnGenerate/index.js
+++ src/components/BtnGenerate/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 
 const BtnGenerate = () => {
-    return <button className="btn-generate"></button>;
+    return <button className="btn-generate">GENERATE PASSWORD</button>;
 }
 
 export default BtnGenerate;
